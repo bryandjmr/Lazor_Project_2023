1728d20c9b4bd92f0c71b1dcd4246356674de762
bryandjmr <bmunozr1@jhu.edu>
2023-11-01 12:59:42 -0400

    Adding Potential Options for block type approach
    I put an option 1 where we make separate subclasses for the blocks, and an option 2 where we just assign type of block as a property of block class and do a bigger function in the block class.

--- Lazor_Project.py
+++ Lazor_Project.py
@@ -1,11 +1,44 @@
 
-#maybe create subclasses that react to the lazor differently 
-#or build in that into the block class
+#maybe create subclasses that react to the lazor differently (option 1)
+#or build in that into the block class (option 2)
 
 class block:
     def __init__(self, position, type):
         self.p = position
         self.t = type
+
+    #option 2
+    def lazor_interaction(self,lazor):
+        if self.t == 0: #opaque
+            pass
+        elif self.t == 1: #reflect
+            pass
+        elif self.t == 2: #refract
+            pass
+
+
+#option 1
+class opaque_block(block):
+    def __init__(self, position, type):
+        super.__init__(self, position, type)
+    
+    def lazor_interaction(self,lazor):
+        pass
+
+
+class reflect_block(block):
+    def __init__(self, position, type):
+        super.__init__(self, position, type)
+    
+    def lazor_interaction(self,lazor):
+        pass
+
+class refract_block(block):
+    def __init__(self, position, type):
+        super.__init__(self, position, type)
+    
+    def lazor_interaction(self,lazor):
+        pass
 
 
 
