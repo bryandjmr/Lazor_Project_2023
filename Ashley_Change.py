53e4d8ce93f5ea5ceaf5318e5c0e8d0b40f04a7b
Ashley T <ltang27@jhu.edu>
2023-11-11 15:51:59 -0500

    update parse function

--- Ashley_Change.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 11 14:28:53 2023
-
-@author: ashleytang
-"""
-
--- Lazor_Project.py
+++ Lazor_Project.py
@@ -1,59 +1,74 @@
+class Block:
+    def __init__(self, block_type, position, fixed=False):
+        self.block_type = block_type
+        self.position = position
+        self.fixed = fixed
+    def interact_with_laser(self, laser):
+        if self.block_type == 'reflect':
+            laser.reflect()  # Reflect method needs to be defined in Laser class
+            return [laser], []  # Only one laser continues, no new laser created.
+        elif self.block_type == 'opaque':
+            return [], []  # Laser stops, no lasers continue.
+        elif self.block_type == 'refract':
+            through_laser = Laser(laser.position, laser.direction)
+            reflect_laser = Laser(laser.position, self._reflect_direction(laser.direction))
+            return [through_laser], [reflect_laser]
+        return [laser], []
+    def _reflect_direction(self, direction):
+        vx, vy = direction
+        return (-vy, vx)  # For a 90-degree reflection
 
-#maybe create subclasses that react to the lazor differently (option 1)
-#or build in that into the block class (option 2)
+def parse_bff(filename):
 
-class block:
-    def __init__(self, position, type):
-        self.p = position
-        self.t = type
+    with open(filename, 'r') as file:
+        lines = file.readlines()
 
-    #option 2
-    def lazor_interaction(self,lazor):
-        if self.t == 0: #opaque
-            pass
-        elif self.t == 1: #reflect
-            pass
-        elif self.t == 2: #refract
-            pass
+    # Initializing lists to hold various elements
+    grid = []
+    fixed_blocks = []
+    movable_blocks = []
+    lasers = []
+    points = []
 
+    # Flags to know when to start parsing which part
+    parsing_grid = False
 
-#option 1
-class opaque_block(block):
-    def __init__(self, position, type):
-        super.__init__(self, position, type)
+    for line in lines:
+        line = line.strip()
+
+        if line == 'GRID START':
+            parsing_grid = True
+
+        elif line == 'GRID STOP':
+            parsing_grid = False
+            #parsing_blocks = True  # Assuming blocks are listed right after the grid
+
+        elif parsing_grid and line:
+            grid.append(list(line))
+
+        elif line.startswith(('A', 'B', 'C')):  # Assuming 'A', 'B', 'C' are blocks
+                block_type, quantity = line.split()
+                for _ in range(int(quantity)):
+                   movable_blocks.append(Block(block_type, None))  # None for position since it's movable
+                   
+        elif line.startswith('L'):
+            line = line[1:]
+            x, y, dx, dy = map(int,line.split())
+            lasers.append((x, y, dx, dy))
     
-    def lazor_interaction(self,lazor):
-        pass
+        elif line.startswith('P'):
+            line = line[1:]
+            if line:  # Assuming points lines are not empty
+                points.append(tuple(map(int, line.split())))
 
+    # Convert grid symbols to Block objects for fixed blocks
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell in 'ABC':
+                fixed_blocks.append(Block(cell, (x, y), True))
+                grid[y][x] = 'o'  # Optional: Replace the block symbols with something else like 'o'
+        
+    return grid, movable_blocks, fixed_blocks, lasers, points
 
-class reflect_block(block):
-    def __init__(self, position, type):
-        super.__init__(self, position, type)
-    
-    def lazor_interaction(self,lazor):
-        pass
-
-class refract_block(block):
-    def __init__(self, position, type):
-        super.__init__(self, position, type)
-    
-    def lazor_interaction(self,lazor):
-        pass
-
-
-
-class lazor:
-    def __init__(self,direction):
-        self.d = direction
-
-
-class grid:
-    def __init__(self,board_file):
-        self.board = self.read_board_file(board_file)
-
-    def read_board_file(self, board_file):
-        pass
-
-#creating a function that will run until it solves the lazor problem
-def lazor_solver(board_file):
-    pass
+if __name__ == "__main__":
+    grid, movable_blocks, fixed_blocks, lasers, target_points = parse_bff('tiny_5.bff')
